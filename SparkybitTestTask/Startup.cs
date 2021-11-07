28300c54f6edb599f59e43bd669d9e07d97435b1
LIUBOMYR-HOLKO <golko04102001@gmail.com>
2021-11-07 11:46:45 +0200

    connected services

--- SparkybitTestTask/Startup.cs
+++ SparkybitTestTask/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using SparkybitTestTask.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,6 +33,8 @@ namespace SparkybitTestTask
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SparkybitTestTask", Version = "v1" });
             });
+
+            services.AddCustomServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
