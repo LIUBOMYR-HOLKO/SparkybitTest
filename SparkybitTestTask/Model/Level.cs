25333ec695de7f66a3a738a1ef4c0a0e4cd5c608
LIUBOMYR-HOLKO <golko04102001@gmail.com>
2021-11-07 21:44:02 +0200

    added model for logs

---
+++ SparkybitTestTask/Model/Level.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SparkybitTestTask.Model
+{
+    public enum Level
+    {
+        Info = 0,
+        Warn = 1,
+        Error = 2
+    }
+}
---
+++ SparkybitTestTask/Model/Logs.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SparkybitTestTask.Model
+{
+    [BsonIgnoreExtraElements]
+    public class Logs
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        [BsonElement("date-time")]
+        public DateTime DateTime { get; set; }
+        [BsonElement("level")]
+        public Level Level { get; set; }
+        [BsonElement("title")]
+        public string Title { get; set; }
+        [BsonElement("description")]
+        public string Description { get; set; }
+    }
+}
