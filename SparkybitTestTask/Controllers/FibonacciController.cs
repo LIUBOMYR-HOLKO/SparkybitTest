809fd6cdb65fb6168fff3d135aa73fc414a56edc
LIUBOMYR-HOLKO <golko04102001@gmail.com>
2021-11-07 11:47:18 +0200

    added controller

---
+++ SparkybitTestTask/Controllers/FibonacciController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SparkybitTestTask.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SparkybitTestTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FibonacciController : ControllerBase
+    {
+        private readonly IFibonacciService _fibonacciService;
+        private readonly IFileService _fileService;
+        private static List<List<int>> list = new List<List<int>>();
+
+        public FibonacciController(IFibonacciService fibonacciService, IFileService fileService)
+        {
+            _fibonacciService = fibonacciService;
+            _fileService = fileService;
+        }
+
+        [HttpGet]
+        public List<List<int>> GetRows(string fileName)
+        {
+            list = _fileService.ReadFromFile(fileName);
+            return list;
+        }
+
+        [HttpPost]
+        public void WriteRows(string fileName)
+        {
+            _fileService.WriteToFile(fileName, list);
+        }
+
+        [HttpPut]
+        public void ReverseFibonacciRows()
+        {
+            _fibonacciService.DoChanges(list);
+        }
+    }
+}
